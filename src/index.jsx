18a8501e7762d71a08c7c9aa4a08a26550d836eb
Divyam Kumar Sinha <divyam150296@gmail.com>
2025-02-28 12:56:31 +0530

    Update the app setting

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
@@ -7,9 +7,9 @@ import App from './App.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log(root)
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
