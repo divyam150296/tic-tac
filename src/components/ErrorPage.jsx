5da77f7e9b5302f8785ccf04fe323386a03e167b
Divyam Kumar Sinha <divyam150296@gmail.com>
2025-04-24 11:29:18 +0530

    Dynamic route related changes

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import NavigatorSection from './Navigator.jsx'
+
+export default function ErrorPage () {
+  return (
+    <>
+        <NavigatorSection />
+        <div>Here is the error page you are seeing</div>
+    </>
+  )
+}
